a47278cf1eb024196665f55e5b47355c321472c2
iibrahimli <imranibrahimli98@gmail.com>
2019-08-26 14:50:37 +0400

    initial commit

---
+++ differential_evolution.cpp
@@ -0,0 +1,1 @@
+#include "differential_evolution.h"
---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <iomanip>
+#include "differential_evolution.h"
+
+using std::cout;
+using de::differential_evolution;

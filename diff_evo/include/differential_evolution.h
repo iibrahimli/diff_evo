5a899e9e26ee00c8944212534196563399cc336e
iibrahimli <imranibrahimli98@gmail.com>
2019-09-17 23:16:00 +0400

    lenovo

--- diff_evo/include/differential_evolution.h
+++ diff_evo/include/differential_evolution.h
@@ -12,7 +12,7 @@ class differential_evolution;
 // functor, inherit and implement operator()
 // function is expected to return a value convertible to double
 template <typename T>
-struct func {
+struct cost {
     virtual T operator() ();
 };
 
@@ -26,7 +26,6 @@ enum mode {
 
 // default values
 const int    DEFAULT_MAX_ITER       = 1000;
-const int    DEFAULT_POP_MULT       = 8;
 const double DEFAULT_MIN_DELTA      = 0.0001;
 const float  DEFAULT_INIT_MUT_PROB  = 0.95;
 const float  DEFAULT_MUT_PROB_DECAY = 0.99;
@@ -36,17 +35,16 @@ const float  DEFAULT_MUT_PROB_DECAY = 0.99;
 
 
 /*
-    algo
+    the class
 */
 template <typename T>
 class differential_evolution {
 private:
 
-    de::func     _target;           // the functor to minimize/maximize
+    de::cost<T>  _target;           // the function to minimize/maximize
     de::mode     _mode;             // minimize / maximize
-    int          _nb_params;        // number of parameters of the function
-    int          _max_iter;         // maximum iterations
-    int          _pop_mult;         // population size is (this) * (# of params)                  (Q)
+    unsigned int _nb_params;        // number of parameters of the function
+    unsigned int _max_iter;         // maximum iterations
     int          _pop_size;         // population size
     T            _min_delta;        // if changes are lower than min delta, algorithm stops
     float        _init_mut_prob;    // initial mutation probability                               (P)
@@ -58,30 +56,29 @@ public:
     /*
         default parameters
     */
-    differential_evolution(de::func  target,
-                           de::mode  op_mode,
-                           int       nb_params,
-                           double    param_min,
-                           double    param_max);
+    differential_evolution(de::cost<T>  target,
+                           de::mode     op_mode,
+                           unsigned int nb_params,
+                           double       param_min,
+                           double       param_max);
 
 
     /*
         user-supplied parameters
     */
-    differential_evolution(de::func  target,
-                           de::mode  op_mode,
-                           int       nb_params,
-                           double    param_min,
-                           double    param_max,
-                           int       pop_mult,
-                           double    min_delta,
-                           float     pert_weight);
+    differential_evolution(de::cost<T>  target,
+                           de::mode     op_mode,
+                           unsigned int nb_params,
+                           double       param_min,
+                           double       param_max,
+                           double       min_delta,
+                           float        pert_weight);
 
 
     /*
         returns number of iterations run
     */
-    int run(int max_iters);
+    unsigned int run(int max_iters);
 
 
 };
